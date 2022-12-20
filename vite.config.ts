1abd31d321a6a30fdc054cbe10d3b38d71539138
Doma <leishenghao@126.com>
2022-12-20 13:55:51 +0800

    chore: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
-  plugins: [react(), svgr()],
-  test: {
-    environment: "happy-dom",
-  },
+  root: "./demo",
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: false,
+    }),
+  ],
 })
