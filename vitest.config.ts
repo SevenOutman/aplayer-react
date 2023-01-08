f67cc88d3de6fa40d8d7bb3c78978ff8581196ac
Doma <leishenghao@126.com>
2023-01-08 12:20:49 +0800

    chore(coverage): change coverage collection range

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   test: {
     environment: "happy-dom",
     coverage: {
+      /** @see https://github.com/bcoe/c8#checking-for-full-source-coverage-using---all */
+      src: ["./src"],
+      all: true,
       reporter: ["text", "lcov"],
     },
   },
